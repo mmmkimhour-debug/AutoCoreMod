8a71966bab2b624a866244e05db4f0c6d8e550a0
mmmkimhour-debug <mmmkimhour@gmail.com>
2026-09-10 08:07:07 +0700

    Update ExampleMod.java

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -17,7 +17,7 @@ import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
 import org.lwjgl.glfw.GLFW;
 
-public class ExampleMod implements ClientModInitializer {
+public class HeavyCoreAutoLoopMod implements ClientModInitializer {
     private static KeyBinding toggleKey;
     private static boolean isAutoScanning = false;
     private int tickCounter = 0;
